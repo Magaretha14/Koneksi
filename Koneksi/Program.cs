ee86efa71dfc3748e46ab2e691a3ca9d66b9b081
Magaretha14 <magaretha.wahyunda@gmail.com>
2022-03-16 14:06:03 +0700

    Add project files.

---
+++ Koneksi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Koneksi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Connecting();
+        }
+
+        public void Connecting()
+        {
+            using (
+                SqlConnection conn = new SqlConnection("data source=LAPTOP-N3UNMQ7P;database=ProdiTI;user ID=sa;Password=Puput1414")
+            )
+            {
+                conn.Open();
+                Console.WriteLine("Berhasil terhubung ke database");
+            }
+        }
+    }
+}
